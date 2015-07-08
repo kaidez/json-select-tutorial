2f4d83ffe98dd52732812ea0d068d7c52a34f326
Kai Gittens <kai.gittens@gmail.com>
2015-07-07 20:49:14 -0400

    get .compile code working

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,25 @@ var teams = {
       "position": "Goalkeeper",
       "number": 1
     }
+  },
+  "acmilan": {
+    "playerOne": {
+      "name": "Pirlo",
+      "position": "Center Forward",
+      "number": 11,
+      "country": "Ivory Coast"
+    },
+    "playerTwo": {
+      "name": "Thibault Courtois",
+      "position": "Goalkeeper",
+      "number": 1
+    }
   }
 };
+// var selector01 = ".acmilan";
+// var selector02 = ".chelsea";
 
-var what = JSONSelect.match("* > .position", teams);
+// var what = JSONSelect.compile(selector02).match(teams);
+// console.log(what);
+
 document.getElementById('container').innerHTML = what;
